13d55baeaee179b7097c47ac04f253cbd25020cc
Zain Fathoni <me@zainf.dev>
2021-11-29 00:23:41 +0800

    Loading Data
    https://remix.run/docs/en/v1/tutorials/blog#loading-data

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,36 @@
+import { Link, useLoaderData } from "remix"
+
+type Post = {
+  slug: string;
+  title: string;
+}
+
+export let loader = () => {
+  let posts: Post[] = [
+    {
+      slug: "my-first-post",
+      title: "My first post",
+    },
+    {
+      slug: "90s-mixtape",
+      title: "A Mixtape I Made Just for You"
+    }
+  ]
+  return posts;
+}
+
 export default function Posts() {
+  let posts = useLoaderData<Post[]>();
   return (
     <div>
       <h1>Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.slug}>
+            <Link to={post.slug}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
